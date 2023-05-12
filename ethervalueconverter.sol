93805efa107ed9085bd22523f8f078b87adbac27
Anjan Gorai <91726494+codecrazeAV@users.noreply.github.com>
2023-05-12 22:44:15 +0530

    Update ethervalueconverter.sol

--- ethervalueconverter.sol
+++ ethervalueconverter.sol
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract convertethervalue {
-    uint256 public weiAmt;
-    uint256 public etherAmt;
-    uint256 public gweiAmt;
-
-    function deposit() public payable {
-        weiAmt = msg.value;
-        etherAmt = weiAmt / 1 ether;
-        gweiAmt = weiAmt / 1 gwei;
+pragma solidity ^0.8.18;
+contract EthvalueConverter{
+    uint public ethamt; uint public gweiamt; uint public weiamt;
+    function EnterEtheramount() public payable {
+        weiamt=msg.value;
+        ethamt=weiamt/1 ether;
+        gweiamt=weiamt/1 gwei;
     }
 }
